34ccefe7c0d30273d4812adc44adc31ee60fa057
Nikola Zivkovic <nzivkovic7@gmail.com>
2023-10-21 04:42:23 +0200

    Initial Commit

---
+++ UkucajSe-23/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UkucajSe-23
+//
+//  Created by Nikola Zivkovic on 10/21/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UkucajSe-23/UkucajSe_23App.swift
@@ -0,0 +1,17 @@
+//
+//  UkucajSe_23App.swift
+//  UkucajSe-23
+//
+//  Created by Nikola Zivkovic on 10/21/23.
+//
+
+import SwiftUI
+
+@main
+struct UkucajSe_23App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
