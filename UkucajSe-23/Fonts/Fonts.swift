d94c17f0b049e785f9cc34121f1657616e026153
Nikola Zivkovic <nzivkovic7@gmail.com>
2023-10-21 04:45:44 +0200

    Initial commit. Fonts and Font extension added to the project.

---
+++ UkucajSe-23/Fonts/Fonts.swift
@@ -0,0 +1,36 @@
+//
+//  Fonts.swift
+//  UkucajSe-Final
+//
+//  Created by Nikola Zivkovic on 10/21/23.
+//
+
+import Foundation
+import SwiftUI
+
+extension Font {
+    static var lexendRegLarge: Font {
+        return .custom("LexendExa-Regular", size: 48, relativeTo: .largeTitle)
+    }
+    
+    static var lexendRegTitle: Font {
+        return .custom("LexendExa-Regular", size: 36, relativeTo: .title)
+    }
+    
+    static var lexendRegTitle2: Font {
+        return .custom("LexendExa-Regular", size: 16, relativeTo: .title2)
+    }
+    
+    static var lexendRegCaption: Font {
+        return .custom("LexendExa-Regular", size: 14, relativeTo: .caption)
+    }
+    
+    static var lexendRegFootnote: Font {
+        return .custom("LexendExa-Regular", size: 14, relativeTo: .footnote)
+    }
+    
+    static var lexendMedTitle2: Font {
+        return .custom("LexendExa-Medium", size: 16, relativeTo: .title2)
+    }
+    
+}
