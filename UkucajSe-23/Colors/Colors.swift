ab4c26a08e291cc542ec52eac4951e4d5f45d53d
Nikola Zivkovic <nzivkovic7@gmail.com>
2023-10-22 02:04:05 +0200

    Added a data provider CityParkingData based upon the City and ParkingZone model. Changed the name of the static vars for colors.

--- UkucajSe-23/Colors/Colors.swift
+++ UkucajSe-23/Colors/Colors.swift
@@ -13,36 +13,20 @@ extension Color {
         return Color("Beige")
     }
     
-    static var blue: Color {
+    static var brightBlue: Color {
         return Color("Blue")
-    }
-    
-    static var blue_30: Color {
-        return Color("Blue").opacity(0.3)
     }
     
     static var brightPink: Color {
         return Color("BrightPink")
     }
     
-    static var brightPink_30: Color {
-        return Color("BrightPink").opacity(0.3)
-    }
-    
-    static var green: Color {
+    static var brightGreen: Color {
         return Color("Green")
     }
     
-    static var green_30: Color {
-        return Color("Green").opacity(0.3)
-    }
-    
-    static var orange: Color {
+    static var brightOrange: Color {
         return Color("Orange")
-    }
-    
-    static var orange_30: Color {
-        return Color("Orange").opacity(0.3)
     }
     
 }
---
+++ UkucajSe-23/Data/CityParkingData.swift
@@ -0,0 +1,68 @@
+//
+//  CityParkingData.swift
+//  UkucajSe-23
+//
+//  Created by Nikola Zivkovic on 10/22/23.
+//
+
+import Foundation
+
+class CityParkingData {
+    static let shared = CityParkingData()
+    
+    let cities: [City] = [
+            City(name: "Nis",
+                 image: "grbNis",
+                 parkingZones: [
+                    ParkingZone(name: "Zona I",
+                                zoneNumber: "9181",
+                                price: 60,
+                                color: .brightPink),
+                    ParkingZone(name: "Zona II",
+                                zoneNumber: "9182",
+                                price: 45,
+                                color: .brightGreen),
+                    ParkingZone(name: "Extra Zona",
+                                zoneNumber: "9180",
+                                price: 90,
+                                color: .brightOrange)
+                 ]
+                ),
+            City(name: "Beograd",
+                 image: "grbBeograd",
+                 parkingZones: [
+                    ParkingZone(name: "Zona I",
+                                zoneNumber: "1bg",
+                                price: 6,
+                                color: .brightPink),
+                    ParkingZone(name: "Zona II",
+                                zoneNumber: "2bg",
+                                price: 8,
+                                color: .brightGreen),
+                    ParkingZone(name: "Zona III",
+                                zoneNumber: "3bg",
+                                price: 7,
+                                color: .brightOrange),
+                    ParkingZone(name: "Zona IV",
+                                zoneNumber: "4bg",
+                                price: 8,
+                                color: .brightBlue)
+                 ]),
+            City(name: "Novi Sad",
+                 image: "grbNoviSad",
+                 parkingZones: [
+                    ParkingZone(name: "Zona I",
+                                zoneNumber: "1ns",
+                                price: 6,
+                                color: .brightPink),
+                    ParkingZone(name: "Zona II",
+                                zoneNumber: "2ns",
+                                price: 8,
+                                color: .brightGreen),
+                    ParkingZone(name: "Zona III",
+                                zoneNumber: "3ns",
+                                price: 7,
+                                color: .brightOrange)
+                 ])
+        ]
+}
